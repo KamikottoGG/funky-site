0c7c5146c8e8c753658bf40e9e22ec0ba956b24b
KamikottoGG <123205307+KamikottoGG@users.noreply.github.com>
2023-12-06 15:45:29 +0200

    фы

--- main.js
+++ main.js
@@ -67,3 +67,33 @@ function botInvite()
 {
     window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1149430767466852406&permissions=8&scope=bot";
 }
+
+
+const titleElement = document.getElementById('animatedTitle');
+let originalText = titleElement.textContent;
+let currentIndex = 0;
+let isUppercase = true;
+
+function isUppercaseLetter(char) {
+    return char === char.toUpperCase();
+}
+
+function animateTitle() {
+    if (isUppercaseLetter(originalText[currentIndex])) {
+        originalText = originalText.substring(0, currentIndex) + originalText[currentIndex].toLowerCase() + originalText.substring(currentIndex + 1);
+    } else {
+        originalText = originalText.substring(0, currentIndex) + originalText[currentIndex].toUpperCase() + originalText.substring(currentIndex + 1);
+    }
+
+    if (currentIndex >= originalText.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+
+    document.title = originalText;
+    setTimeout(animateTitle, 500);
+}
+
+animateTitle(); 
+
